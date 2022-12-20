eb8acb9d9939db99aa3669082d8e90d0c5877351
ImFlexo <97156492+ImFlexo@users.noreply.github.com>
2022-12-20 20:42:14 +0300

    Add files via upload

---
+++ src/project_email_creator/Email.java
@@ -0,0 +1,87 @@
+
+package project_email_creator;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Email {
+    
+    private String  firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private String email;
+    private int defaultpasswordlength = 8;
+    private String company_name = "companyname.co";
+
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Creating Email For:  " + this.firstName + " " + this.lastName);
+        this.department = setDepartment();
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase()+ "." +department + "@"  + company_name;
+        this.password = randomPassword(defaultpasswordlength);
+
+    }
+
+    private String setDepartment() {
+        
+      Scanner sc = new Scanner(System.in);
+      
+char options;
+
+do{
+    System.out.print("Department Codes\nA- Dev\nB- Acc\nC- Net\nEnter Your Department Code: ");
+    
+    options=sc.next().charAt(0);    
+    switch(options){
+        case 'A':
+           return "Dev";
+           
+        case 'B':
+          return "Acc";
+            
+        case 'C':
+           return "Net";
+            
+        default:
+            System.out.println("Invalid character, please try again!");
+        
+    }
+    
+}while(options != 'A' && options!='B'&& options!='C');
+        
+     
+     return "";
+    }
+
+    private String randomPassword(int length) {
+        Random r = new Random();
+
+        String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String Small_chars = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "0123456789";
+        String symbols = "@#$&";
+        String values = Capital_chars + Small_chars + numbers + symbols;
+
+        String password = "";
+        for (int i = 0; i < length; i++) {
+            password = password + values.charAt(r.nextInt(values.length()));
+        }
+        return  password;
+    }
+
+    public String getPassword() {
+        return  password;
+    }
+
+    public String showInfo() {
+        System.out.println("");
+        return "Display Name: " + firstName +" "+ lastName +
+                "\nDepartment: " + this.department +
+                "\nUniversity Email: " + email +
+                "\nUniversity Password: " + this.password;
+    }
+    
+}
---
+++ src/project_email_creator/Project_Email_creator.java
@@ -0,0 +1,29 @@
+
+package project_email_creator;
+
+import java.util.Scanner;
+
+
+public class Project_Email_creator {
+
+   
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+       
+        
+        System.out.print("First Name: ");
+      String first_name = sc.next();
+      
+       System.out.print("Last Name: ");
+       String last_name = sc.next();
+       
+        Email em1 = new Email(first_name,last_name);
+       System.out.println(em1.showInfo());
+        
+      
+        
+    }
+    
+}
